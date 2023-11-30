ceca646f2c72add23ef25ae820b5945b22b7f1f5
ElektrikSpark <elektrikspark@gmail.com>
2023-11-30 06:22:00 -0600

    added canvas client to ctx

--- packages/api/src/router/canvas.ts
+++ packages/api/src/router/canvas.ts
@@ -2,16 +2,15 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
-  createApiClient,
   get_ReadPatient,
   get_SearchPatient,
-} from "../canvas.client";
-import { env } from "../env.mjs";
+  post_CreatePatient,
+} from "../canvas/canvas-client";
 import { createTRPCRouter, protectedCanvasProcedure } from "../trpc";
 
 export const canvasRouter = createTRPCRouter({
   getAllPatients: protectedCanvasProcedure.query(async ({ ctx }) => {
-    const { canvasToken } = ctx;
+    const { api, canvasToken } = ctx;
 
     if (!canvasToken) {
       throw new TRPCError({
@@ -19,15 +18,6 @@ export const canvasRouter = createTRPCRouter({
         message: "Canvas token is missing",
       });
     }
-
-    const api = createApiClient(
-      (method, url) =>
-        fetch(url, {
-          method,
-          headers: { Authorization: `Bearer ${canvasToken}` },
-        }).then((res) => res.json()),
-      env.FUMAGE_BASE_URL,
-    );
 
     try {
       const patientsData = await api.get("/Patient", { query: {} });
@@ -44,7 +34,7 @@ export const canvasRouter = createTRPCRouter({
   getPatient: protectedCanvasProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
-      const { canvasToken } = ctx;
+      const { api, canvasToken } = ctx;
 
       if (!canvasToken) {
         throw new TRPCError({
@@ -53,21 +43,37 @@ export const canvasRouter = createTRPCRouter({
         });
       }
 
-      const api = createApiClient(
-        (method, url) =>
-          fetch(url, {
-            method,
-            headers: { Authorization: `Bearer ${canvasToken}` },
-          }).then((res) => res.json()),
-        env.FUMAGE_BASE_URL,
-      );
-
       try {
         const patientData = await api.get("/Patient/{patient_id}", {
           path: { patient_id: input.id },
         });
         const validatedData = get_ReadPatient.response.parse(patientData);
         return validatedData;
+      } catch (error) {
+        // Handle any other errors
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An error occurred while fetching patient data",
+        });
+      }
+    }),
+  createPatient: protectedCanvasProcedure
+    .input(post_CreatePatient.parameters)
+    .mutation(async ({ ctx, input }) => {
+      const { api, canvasToken } = ctx;
+      const { body } = input;
+
+      if (!canvasToken) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "Canvas token is missing",
+        });
+      }
+
+      try {
+        return await api.post("/Patient", {
+          body,
+        });
       } catch (error) {
         // Handle any other errors
         throw new TRPCError({
