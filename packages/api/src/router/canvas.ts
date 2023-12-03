cc11bf8e38a647a02f8096945a7b6932a9dacba3
ElektrikSpark <elektrikspark@gmail.com>
2023-12-03 09:42:52 -0600

    patient can provide their subscriber id and payor id to get insurance in canvas

--- packages/api/src/router/canvas.ts
+++ packages/api/src/router/canvas.ts
@@ -2,9 +2,11 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
+  get_ReadConsent,
   get_ReadPatient,
   get_SearchPatient,
   post_CreateConsent,
+  post_CreateCoverage,
   post_CreatePatient,
 } from "../canvas/canvas-client";
 import { createTRPCRouter, protectedCanvasProcedure } from "../trpc";
@@ -169,7 +171,8 @@ export const canvasRouter = createTRPCRouter({
         const consentData = await api.get("/Consent/{consent_id}", {
           path: { consent_id: id },
         });
-        return consentData;
+        const validatedData = get_ReadConsent.response.parse(consentData);
+        return validatedData;
       } catch (error) {
         // Handle any other errors
         throw new TRPCError({
@@ -201,6 +204,35 @@ export const canvasRouter = createTRPCRouter({
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "An error occurred while fetching consent data",
+        });
+      }
+    }),
+
+  // Coverage procedures
+  submitCoverage: protectedCanvasProcedure
+    .input(post_CreateCoverage.parameters)
+    .mutation(async ({ ctx, input }) => {
+      const { api, canvasToken } = ctx;
+      const { query, body } = input;
+
+      if (!canvasToken) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "Canvas token is missing",
+        });
+      }
+
+      try {
+        const coverageData = await api.post("/Coverage", {
+          query,
+          body,
+        });
+        return coverageData;
+      } catch (error) {
+        // Handle any other errors
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An error occurred while fetching coverage data",
         });
       }
     }),
