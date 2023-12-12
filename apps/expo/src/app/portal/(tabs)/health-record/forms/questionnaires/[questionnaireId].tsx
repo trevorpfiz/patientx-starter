2303cf95933112636141853344ac0e07960f40c7
ElektrikSpark <elektrikspark@gmail.com>
2023-12-11 18:44:59 -0600

    basic forms

--- apps/expo/src/app/portal/(tabs)/health-record/forms/questionnaires/[questionnaireId].tsx
+++ apps/expo/src/app/portal/(tabs)/health-record/forms/questionnaires/[questionnaireId].tsx
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
+import { FlashList } from "@shopify/flash-list";
 
+import QuestionItem from "~/components/ui/health-record/question-item";
 import {
   ChatRightHeaderClose,
   MessagesLeftHeaderBack,
@@ -26,7 +28,7 @@ export default function QuesitonnairePage() {
   }
 
   return (
-    <View>
+    <View className="flex-1 bg-gray-100">
       <Stack.Screen
         options={{
           title: questionnaireId,
@@ -34,8 +36,33 @@ export default function QuesitonnairePage() {
           headerRight: () => <ChatRightHeaderClose />,
         }}
       />
-      <Text>Questionnaire Page</Text>
-      <Text>{questionnaireId}</Text>
+      <Text className="text-lg font-semibold">
+        {data?.questionnaire ?? "unknown questionnaire"}
+      </Text>
+      <Text>{data?.status ?? "unknown"}</Text>
+      <Text>{data?.authored ?? "unknown"}</Text>
+      <FlashList
+        data={data?.item}
+        renderItem={({ item, index }) => (
+          <QuestionItem
+            text={item.text ?? "unknown question"}
+            answer={
+              item.answer?.[0]?.valueCoding?.display ??
+              item.answer?.[0]?.valueString ??
+              "unknown answer"
+            }
+            first={index === 0}
+            last={index === (data?.item?.length ?? 0) - 1}
+          />
+        )}
+        estimatedItemSize={100}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={{
+          paddingBottom: 16,
+          // paddingTop: 16,
+          // paddingHorizontal: 16,
+        }}
+      />
     </View>
   );
 }
