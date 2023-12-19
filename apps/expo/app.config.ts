c585a40e3edbe35f0c90de8765276272bd3a6712
ElektrikSpark <elektrikspark@gmail.com>
2023-12-19 04:38:55 -0600

    working on getting eas build working

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,8 +1,8 @@
 import type { ExpoConfig } from "@expo/config";
 
 const defineConfig = (): ExpoConfig => ({
-  name: "expo",
-  slug: "expo",
+  name: "canvas-fhir-api-telehealth-starter",
+  slug: "canvas-fhir",
   scheme: "expo",
   version: "0.1.0",
   orientation: "portrait",
@@ -33,6 +33,7 @@ const defineConfig = (): ExpoConfig => ({
       projectId: "73f7f302-6adf-4a66-a030-6f9acba0e8b9",
     },
   },
+  owner: "trust-the-process",
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
--- apps/expo/src/app/onboarding/(modals)/pdf.tsx
+++ apps/expo/src/app/onboarding/(modals)/pdf.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, Text, useWindowDimensions } from "react-native";
+
+// import Pdf from "react-native-pdf";
+
+const source = {
+  uri: "https://samples.leanpub.com/thereactnativebook-sample.pdf",
+  cache: true,
+};
 
 export default function PDFPage() {
+  const { width, height } = useWindowDimensions();
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>PDFPage</Text>
-      </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>PDF Page</Text>
+      {/* <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={(error) => {
+          console.log(error);
+        }}
+        onPressLink={(uri) => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+        style={{ flex: 1, width, height }}
+      /> */}
     </SafeAreaView>
   );
 }
