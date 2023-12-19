d8c9533e9a4c68e28f238af12defc60009a286d0
ElektrikSpark <elektrikspark@gmail.com>
2023-12-19 04:55:20 -0600

    test if this fixes prod api

--- apps/nextjs/src/app/(landing)/page.tsx
+++ apps/nextjs/src/app/(landing)/page.tsx
@@ -1,14 +1,7 @@
-import { Suspense } from "react";
 import Link from "next/link";
 
 import { AllPatients } from "./_components/all-patients";
-import { AuthShowcase } from "./_components/auth-showcase";
 import { Patient } from "./_components/patient";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
 
 export const runtime = "edge";
 
@@ -19,30 +12,14 @@ export default function HomePage() {
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-pink-400">T3</span> Turbo
         </h1>
-        <Patient />
-        <AllPatients />
         <Link href="/onboarding">
           <h2 className="text-blue-500">Onboarding</h2>
         </Link>
         <Link href="/dashboard">
           <h2 className="text-blue-500">Dashboard</h2>
         </Link>
-        <AuthShowcase />
-
-        <CreatePostForm />
-        <div className="h-[40vh] w-full max-w-2xl overflow-y-scroll">
-          <Suspense
-            fallback={
-              <div className="flex w-full flex-col gap-4">
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-              </div>
-            }
-          >
-            <PostList />
-          </Suspense>
-        </div>
+        <Patient />
+        <AllPatients />
       </div>
     </main>
   );
