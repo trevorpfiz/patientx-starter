730f8a0836c4edfd755b0c828e30339c56bb4624
ElektrikSpark <elektrikspark@gmail.com>
2023-12-18 23:52:26 -0600

    practitioner router

--- packages/api/src/router/practitioner.ts
+++ packages/api/src/router/practitioner.ts
@@ -1,6 +1,9 @@
 import { TRPCError } from "@trpc/server";
 
-import { get_SearchPractitioner } from "../canvas/canvas-client";
+import {
+  get_ReadPractitioner,
+  get_SearchPractitioner,
+} from "../canvas/canvas-client";
 import { createTRPCRouter, protectedCanvasProcedure } from "../trpc";
 
 export const practitionerRouter = createTRPCRouter({
@@ -19,6 +22,41 @@ export const practitionerRouter = createTRPCRouter({
       // Validate response
       const validatedData =
         get_SearchPractitioner.response.parse(practitionerData);
+
+      // Check if response is OperationOutcome
+      if (validatedData?.resourceType === "OperationOutcome") {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `FHIR OperationOutcome Error: ${validatedData.issue
+            .map(
+              (issue) =>
+                `${issue.severity}: ${issue.code}, ${issue.details?.text}`,
+            )
+            .join("; ")}`,
+        });
+      }
+
+      return validatedData;
+    }),
+  getPractitioner: protectedCanvasProcedure
+    .input(get_ReadPractitioner.parameters)
+    .query(async ({ ctx, input }) => {
+      const { api } = ctx;
+      const { path } = input;
+
+      // get /Practitioner{id}
+      const practitionerData = await api.get(
+        "/Practitioner/{practitioner_a_id}",
+        {
+          path: {
+            practitioner_a_id: path.practitioner_a_id,
+          },
+        },
+      );
+
+      // Validate response
+      const validatedData =
+        get_ReadPractitioner.response.parse(practitionerData);
 
       // Check if response is OperationOutcome
       if (validatedData?.resourceType === "OperationOutcome") {
