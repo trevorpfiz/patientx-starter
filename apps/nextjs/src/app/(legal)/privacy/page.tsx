2ae1dccb4e2664ee18b26a6a26563a3f44e8a237
Zachary Pfizenmaier <ztpfizcode@gmail.com>
2023-12-28 23:19:23 -0600

    added privacy policy

---
+++ apps/nextjs/src/app/(legal)/privacy/page.tsx
@@ -0,0 +1,31 @@
+const PrivacyPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        For the purpose of this hackathon, our application does not collect or
+        store any real personal data. All data used during the event, including
+        data used by judges or any other participants, is entirely fictitious
+        and solely for demonstration purposes.
+      </p>
+      <p>
+        We are committed to ensuring the privacy and security of any data used
+        within our application. No real user data is collected, processed, or
+        stored at any point during the operation of this application for the
+        hackathon.
+      </p>
+      <p>
+        This privacy policy is effective as of December 28, 2023 and will remain
+        in effect except with respect to any changes in its provisions in the
+        future, which will be in effect immediately after being posted on this
+        page.
+      </p>
+      <p>
+        For any questions or suggestions regarding our privacy policy, please
+        contact us at tpfiz1@gmail.com
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPage;
